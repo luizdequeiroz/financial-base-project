93b58988f83ca216f5867ebc0751756edb884326
Luiz Viana de Queiroz Neto <luizdequeiroz@outlook.com.br>
2019-01-16 14:56:11 -0300

    data annotation para requerimento de email no model User.

--- BaseProj.Core/Entities/User.cs
+++ BaseProj.Core/Entities/User.cs
@@ -1,7 +1,10 @@
-﻿namespace BaseProj.Core.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BaseProj.Core.Entities
 {
     public class User : GenericEntity
     {
+        [Required(ErrorMessage = "E-mail obrigatório.")]
         public string Email { get; set; }
         public string Password { get; set; }
     }
